e5b843e1a127023d3c10cc78ea63a097ab5c9f85
bibhukjha <msgbox.ubibhu@gmail.com>
2022-03-16 15:29:56 +0530

    Exception handling using NULL e

--- src/com/harman/batch4/Check.java
+++ src/com/harman/batch4/Check.java
@@ -4,13 +4,20 @@ public class Check {
 
     public static void main(String[] args) {
         try{
-            int x=10, y = 0,z ;
-            z=x/y;
-            System.out.println(z);
+
+            String name = "Hello";
+            System.out.println(name.length());
+            //int x=10, y = 0,z ;
+            //z=x/y;
+            //System.out.println(z);
         }
         catch (ArithmeticException e)
         {
             System.out.println("Divisiblity by ZERO not possible");
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Null value is not acceptable");
         }
 
     }
